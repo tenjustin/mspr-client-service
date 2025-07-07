06752b54fb90aa5a159adb849a52033b640fa551
Bathieuu <mathieu.broccoli@gmail.com>
2025-07-07 17:04:34 +0200

    feat: database

---
+++ src/Kawa.ClientService.Api/Database/ClientsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Kawa.ClientService.ServiceDefaults.Database.Models;
+
+namespace Kawa.ClientService.ServiceDefaults.Database
+{
+
+
+    public class ClientsDbContext : DbContext
+    {
+        public ClientsDbContext(DbContextOptions<ClientsDbContext> options) : base(options) { }
+
+        public DbSet<Client> Clients { get; set; } = default!;
+    }
+
+}
