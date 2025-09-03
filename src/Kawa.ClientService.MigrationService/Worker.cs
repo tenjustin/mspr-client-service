950b1c7f91a7cc92e0af43a49ee5daa2287b562f
Justin Tene <tene.justin@gmail.com>
2025-09-03 15:57:44 +0200

    feat: add migration service

---
+++ src/Kawa.ClientService.MigrationService/Worker.cs
@@ -0,0 +1,43 @@
+using System.Diagnostics;
+using Kawa.ClientService.ServiceDefaults.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace SupportTicketApi.MigrationService;
+
+public class Worker(
+    IServiceProvider serviceProvider,
+    IHostApplicationLifetime hostApplicationLifetime) : BackgroundService
+{
+    public const string ActivitySourceName = "Migrations";
+    private static readonly ActivitySource s_activitySource = new(ActivitySourceName);
+
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        using var activity = s_activitySource.StartActivity("Migrating database", ActivityKind.Client);
+
+        try
+        {
+            using var scope = serviceProvider.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ClientsDbContext>();
+
+            await RunMigrationAsync(dbContext, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            activity?.AddException(ex);
+            throw;
+        }
+
+        hostApplicationLifetime.StopApplication();
+    }
+
+    private static async Task RunMigrationAsync(ClientsDbContext dbContext, CancellationToken cancellationToken)
+    {
+        var strategy = dbContext.Database.CreateExecutionStrategy();
+        await strategy.ExecuteAsync(async () =>
+        {
+            // Run migration in a transaction to avoid partial migration if it fails.
+            await dbContext.Database.MigrateAsync(cancellationToken);
+        });
+    }
+}
