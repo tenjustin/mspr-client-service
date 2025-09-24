31c374cea8fe67ee6eb8e7d754c5c4e73148ff45
Justin Tene <tene.justin@gmail.com>
2025-09-24 22:32:20 +0200

    test: crud test

--- test/Kawa.ClientService.Api.Tests/CustomWebApplicationFactory.cs
+++ test/Kawa.ClientService.Api.Tests/CustomWebApplicationFactory.cs
@@ -1,14 +1,59 @@
+using Kawa.ClientService.ServiceDefaults.Database;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+using Testcontainers.RabbitMq;
 
 namespace Kawa.ClientService.Api.Tests;
 
-public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IDisposable
 {
+    private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
+        .WithDatabase("testdb")
+        .WithUsername("postgres")
+        .WithPassword("postgres")
+        .Build();
+
+    private readonly RabbitMqContainer _rabbitMq = new RabbitMqBuilder()
+        .WithImage("rabbitmq:3.11")
+        .WithUsername("guest")
+        .WithPassword("guest")
+        .Build();
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        // You can configure the web host here if needed
-        // For example, you can set up test-specific configurations or services
+        _postgres.StartAsync().GetAwaiter().GetResult();
+        _rabbitMq.StartAsync().GetAwaiter().GetResult();
+
         base.ConfigureWebHost(builder);
+
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbContextOptions<ClientsDbContext>));
+
+            if (dbContextDescriptor != null)
+            {
+                services.Remove(dbContextDescriptor);
+            }
+
+            services.AddDbContext<ClientsDbContext>(options =>
+            {
+                options.UseNpgsql(_postgres.GetConnectionString());
+            });
+
+            var dbContext = services.BuildServiceProvider()
+                .GetRequiredService<ClientsDbContext>();
+            dbContext.Database.EnsureCreated();
+        });
+    }
+
+    public void Dispose()
+    {
+        _postgres.DisposeAsync().GetAwaiter().GetResult();
     }
 }
